7fabfc6fe836e0d99a323bf0c007cf72ff784d39
olzhas2102 <olzhas2102@yahoo.com>
2018-05-02 10:36:55 +0200

    setup - layout and header

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  );
+};
+
+export default App;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
